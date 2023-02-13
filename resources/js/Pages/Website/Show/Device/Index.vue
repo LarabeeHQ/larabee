208681c268847890225f314ccab50fb3b2a2603a
Paulo Castellano <paulofcastellano@gmail.com>
2023-02-13 10:47:58 -0300

    skiping from bots

--- resources/js/Pages/Website/Show/Device/Index.vue
+++ resources/js/Pages/Website/Show/Device/Index.vue
@@ -8,7 +8,7 @@ import Screen from "@/Pages/Website/Show/Device/Screen.vue";
 import Device from "@/Pages/Website/Show/Device/Device.vue";
 
 const tabs = ["devices", "browsers", "OS", "language", "screen"];
-const tab = ref(tabs[0]);
+const tab = ref("language");
 
 const { dateRange, website } = defineProps({
     dateRange: Object,
