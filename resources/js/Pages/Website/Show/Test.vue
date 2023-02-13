d569d94a8c613fbed98f59470d57523fa18580dd
Paulo Castellano <paulofcastellano@gmail.com>
2023-02-12 21:44:20 -0300

    skiping from bots

--- resources/js/Pages/Website/Show/Test.vue
+++ resources/js/Pages/Website/Show/Test.vue
@@ -1,3 +1,16 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+const { dateRange, website } = defineProps({
+    dateRange: Object,
+    website: Object,
+});
+
+onMounted(() => {
+    console.log(dateRange);
+    console.log(website);
+});
+</script>
 <template>
     <div class="text-white">asa</div>
 </template>
