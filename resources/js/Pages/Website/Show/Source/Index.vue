ad0018951ae57abd9a8529af617cc6024718eac4
Paulo Castellano <paulofcastellano@gmail.com>
2023-02-12 23:05:49 -0300

    skiping from bots

--- resources/js/Pages/Website/Show/Source/Index.vue
+++ resources/js/Pages/Website/Show/Source/Index.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from "vue";
-
+import Tab from "@/Components/Tab.vue";
 import Referrer from "@/Pages/Website/Show/Source/Referrer.vue";
 import Medium from "@/Pages/Website/Show/Source/Medium.vue";
 import Source from "@/Pages/Website/Show/Source/Source.vue";
@@ -31,6 +31,7 @@ const setTab = (value) => {
 
 <template>
     <div class="col-span-12 lg:col-span-6 card p-4 min-h-[450px]">
+        <Tab :tabs="tabNames" @update="setTab" />
         <div class="mt-4">
             <component
                 :is="tabs[tab]"
