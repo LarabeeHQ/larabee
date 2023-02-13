6c7f906c9a103459336a3ffdf79e7232ff7520a6
Paulo Castellano <paulofcastellano@gmail.com>
2023-02-13 11:09:44 -0300

    skiping from bots

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -4,8 +4,8 @@ import Banner from "@/Components/Banner.vue";
 </script>
 
 <template>
-    <Header />
-    <!-- <Banner /> -->
+    <!-- <Header /> -->
+    <Banner />
     <div class="min-h-screen bg-zinc-100 dark:bg-zinc-900">
         <main class="max-w-6xl mx-auto sm:px-6 lg:px-8 space-y-4 py-12">
             <slot />
