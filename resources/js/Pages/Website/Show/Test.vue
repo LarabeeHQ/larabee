6ee6e5793b6675ed7fd6ca8978cb2c135d7b651b
Paulo Castellano <paulofcastellano@gmail.com>
2023-02-12 22:34:13 -0300

    skiping from bots

--- resources/js/Pages/Website/Show/Test.vue
+++ resources/js/Pages/Website/Show/Test.vue
@@ -15,7 +15,7 @@ const loadData = () => {
             params: {
                 start: dateRange.start,
                 end: dateRange.end,
-                metric: "utm-contents",
+                metric: "utm-campaigns",
             },
         })
         .then((response) => {
