0954a4c17a4c05b52f6ab68b0419df2a219f8bac
Paulo Castellano <paulofcastellano@gmail.com>
2023-02-12 21:47:36 -0300

    skiping from bots

--- resources/js/Pages/Website/Show/Test.vue
+++ resources/js/Pages/Website/Show/Test.vue
@@ -1,14 +1,34 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, watch } from "vue";
 
 const { dateRange, website } = defineProps({
     dateRange: Object,
     website: Object,
 });
 
+const data = ref(null);
+
+const loadData = () => {
+    axios
+        .get(route("websites.statistics", website.id), {
+            params: {
+                start: dateRange.start,
+                end: dateRange.end,
+                metric: "utm-sources",
+            },
+        })
+        .then((response) => {
+            data.value = response.data;
+            console.log(data.value);
+        });
+};
+
 onMounted(() => {
-    console.log(dateRange);
-    console.log(website);
+    loadData();
+});
+
+watch(dateRange, (value) => {
+    loadData();
 });
 </script>
 <template>
