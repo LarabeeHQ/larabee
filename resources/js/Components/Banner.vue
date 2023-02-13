54aacad47b830915c446c129b5332fbc77b14269
Paulo Castellano <paulofcastellano@gmail.com>
2023-02-13 11:14:59 -0300

    skiping from bots

--- resources/js/Components/Banner.vue
+++ resources/js/Components/Banner.vue
@@ -1,116 +1,106 @@
+<script setup>
+import { computed, ref, watch } from "vue";
+import { usePage } from "@inertiajs/vue3";
+
+const show = ref(true);
+const style = computed(
+    () => usePage().props.jetstream.flash?.bannerStyle || "success"
+);
+const message = computed(() => usePage().props.jetstream.flash?.banner || "");
+
+watch(message, async () => {
+    show.value = true;
+});
+</script>
+
 <template>
-    <div
-        v-if="show && message"
-        aria-live="assertive"
-        class="fixed inset-0 flex px-4 py-6 pointer-events-none sm:p-6 sm:items-start z-50"
-    >
+    <div>
         <div
-            class="w-full flex flex-col absolute bottom-0 mb-4 items-center space-y-4 justify-center"
+            v-if="show && message"
+            :class="{
+                'bg-indigo-500': style == 'success',
+                'bg-red-700': style == 'danger',
+            }"
         >
-            <transition
-                enter-active-class="transform ease-out duration-300 transition"
-                enter-from-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
-                enter-to-class="translate-y-0 opacity-100 sm:translate-x-0"
-                leave-active-class="transition ease-in duration-100"
-                leave-from-class="opacity-100"
-                leave-to-class="opacity-0"
-            >
-                <div
-                    class="max-w-lg w-full bg-gray-900 dark:bg-white shadow-2xl rounded-lg pointer-events-auto ring-1 ring-black ring-opacity-5 overflow-hidden"
-                >
-                    <div class="px-4 py-4">
-                        <div class="flex items-center">
-                            <div class="flex-shrink-0">
-                                <CheckCircleIcon
-                                    v-if="style == 'success'"
-                                    class="h-5 w-5 text-green-400 stroke-2"
-                                    aria-hidden="true"
-                                />
-                                <XCircleIcon
-                                    v-if="style == 'danger'"
-                                    class="h-5 w-5 text-red-400 stroke-2"
-                                    aria-hidden="true"
-                                />
-                                <InformationCircleIcon
-                                    v-if="style == 'info'"
-                                    class="h-5 w-5 text-blue-400 stroke-2"
-                                    aria-hidden="true"
-                                />
-                                <ExclamationCircleIcon
-                                    v-if="style == 'warning'"
-                                    class="h-5 w-5 text-yellow-400 stroke-2"
-                                    aria-hidden="true"
-                                />
-                            </div>
-                            <div
-                                class="ml-2 w-0 flex-1 text-sm text-gray-300 dark:text-gray-800 font-semibold"
+            <div class="max-w-screen-xl mx-auto py-2 px-3 sm:px-6 lg:px-8">
+                <div class="flex items-center justify-between flex-wrap">
+                    <div class="w-0 flex-1 flex items-center min-w-0">
+                        <span
+                            class="flex p-2 rounded-lg"
+                            :class="{
+                                'bg-indigo-600': style == 'success',
+                                'bg-red-600': style == 'danger',
+                            }"
+                        >
+                            <svg
+                                v-if="style == 'success'"
+                                class="h-5 w-5 text-white"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
                             >
-                                {{ message }}
-                            </div>
-                            <div class="ml-4 flex-shrink-0 flex">
-                                <button
-                                    @click="show = false"
-                                    class="inline-flex text-gray-500 dark:text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                >
-                                    <span class="sr-only">Close</span>
-                                    <XMarkIcon
-                                        class="h-5 w-5"
-                                        aria-hidden="true"
-                                    />
-                                </button>
-                            </div>
-                        </div>
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                />
+                            </svg>
+
+                            <svg
+                                v-if="style == 'danger'"
+                                class="h-5 w-5 text-white"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+                                />
+                            </svg>
+                        </span>
+
+                        <p class="ml-3 font-medium text-sm text-white truncate">
+                            {{ message }}
+                        </p>
+                    </div>
+
+                    <div class="shrink-0 sm:ml-3">
+                        <button
+                            type="button"
+                            class="-mr-1 flex p-2 rounded-md focus:outline-none sm:-mr-2 transition"
+                            :class="{
+                                'hover:bg-indigo-600 focus:bg-indigo-600':
+                                    style == 'success',
+                                'hover:bg-red-600 focus:bg-red-600':
+                                    style == 'danger',
+                            }"
+                            aria-label="Dismiss"
+                            @click.prevent="show = false"
+                        >
+                            <svg
+                                class="h-5 w-5 text-white"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M6 18L18 6M6 6l12 12"
+                                />
+                            </svg>
+                        </button>
                     </div>
                 </div>
-            </transition>
+            </div>
         </div>
     </div>
 </template>
-
-<script>
-import {
-    XMarkIcon,
-    CheckCircleIcon,
-    XCircleIcon,
-    InformationCircleIcon,
-    ExclamationCircleIcon,
-} from "@heroicons/vue/24/solid";
-
-export default {
-    components: {
-        CheckCircleIcon,
-        XMarkIcon,
-        XCircleIcon,
-        InformationCircleIcon,
-        ExclamationCircleIcon,
-    },
-
-    data() {
-        return {
-            show: true,
-        };
-    },
-
-    watch: {
-        "$page.props.flash": {
-            handler() {
-                this.show = true;
-                setTimeout(() => {
-                    this.show = false;
-                }, 3000);
-            },
-            deep: true,
-        },
-    },
-
-    computed: {
-        style() {
-            return this.$page.props.flash?.bannerStyle || "success";
-        },
-
-        message() {
-            return this.$page.props.flash?.banner || "";
-        },
-    },
-};
-</script>
