da633b982462285130b988873b08656864d8498b
Paulo Castellano <paulofcastellano@gmail.com>
2023-02-14 16:00:44 -0300

    improvement

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,8 +15,6 @@
     @routes
     @vite('resources/js/app.js')
     @inertiaHead
-    <!-- @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
-    @inertiaHead -->
 
     <!-- Stripe -->
     <script src="https://js.stripe.com/v3/" defer></script>
