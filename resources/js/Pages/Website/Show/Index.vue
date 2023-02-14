4de489f46cf678e5d8ef60822b952cf3f4494cd9
Paulo Castellano <paulofcastellano@gmail.com>
2023-02-14 10:45:28 -0300

    improvement

--- resources/js/Pages/Website/Show/Index.vue
+++ resources/js/Pages/Website/Show/Index.vue
@@ -17,6 +17,7 @@ import { onMounted, ref, computed } from "vue";
 const user = computed(() => usePage().props.auth.user);
 const websites = user.value ? user.value.websites : [];
 const website = computed(() => usePage().props.website);
+
 const online = ref(0);
 
 const range = ref({
@@ -151,9 +152,9 @@ onMounted(() => {
     <Head :title="website.name" />
 
     <AuthenticatedLayout>
-        <!-- <div v-if="website.sessions_count >= 1"> -->
-        <div class="flex items-center justify-between mb-2">
-            <!-- <div class="min-w-0 flex-1">
+        <div v-if="website.sessions_count >= 1">
+            <div class="flex items-center justify-between mb-2">
+                <!-- <div class="min-w-0 flex-1">
                 <div class="hidden space-x-4 sm:-my-px sm:flex items-center">
                     <div v-if="user">
                         <Menu as="div" class="relative inline-block text-left">
@@ -230,7 +231,7 @@ onMounted(() => {
                 </div>
             </div> -->
 
-            <Menu as="div" class="relative inline-block text-left">
+                <!-- <Menu as="div" class="relative inline-block text-left">
                 <div>
                     <MenuButton class="inline-flex w-full btn-dropdown">
                         {{
@@ -362,39 +363,23 @@ onMounted(() => {
                         </div>
                     </MenuItems>
                 </transition>
-            </Menu>
-        </div>
-
-        <div class="space-y-4">
-            <Overview
-                key="overviewIndex"
-                :dateRange="range"
-                :website="website"
-            />
-
-            <div class="grid grid-cols-12 gap-4">
-                <Source
-                    key="sourceIndex"
-                    :dateRange="range"
-                    :website="website"
-                />
-                <Page key="pageIndex" :dateRange="range" :website="website" />
+            </Menu> -->
             </div>
 
-            <div class="grid grid-cols-12 gap-4">
-                <Location
-                    key="locationIndex1"
-                    :dateRange="range"
-                    :website="website"
-                />
-                <Device
-                    key="deviceIndex"
-                    :dateRange="range"
-                    :website="website"
-                />
+            <div class="space-y-4">
+                <Overview :dateRange="range" :website="website" />
+
+                <div class="grid grid-cols-12 gap-4">
+                    <Source :dateRange="range" :website="website" />
+                    <Page :dateRange="range" :website="website" />
+                </div>
+
+                <div class="grid grid-cols-12 gap-4">
+                    <Location :dateRange="range" :website="website" />
+                    <Device :dateRange="range" :website="website" />
+                </div>
             </div>
         </div>
-        <!-- </div>
-        <NoDataPlaceholder v-else :website="website" /> -->
+        <NoDataPlaceholder v-else :website="website" />
     </AuthenticatedLayout>
 </template>
