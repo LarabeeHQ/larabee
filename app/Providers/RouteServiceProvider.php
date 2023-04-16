431fe917452ed3d1c1a02210d91e595371a864bd
Paulo Castellano <paulofcastellano@gmail.com>
2023-04-16 20:32:22 -0300

    small improvements

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/app/analytics';
+    public const HOME = '/analytics';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
