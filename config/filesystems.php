0859348734ae0cbf318c56e8ac81f06c3f21cf1c
Paulo Castellano <paulofcastellano@gmail.com>
2023-04-15 23:08:36 -0300

    composer update

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,6 +56,15 @@ return [
             'throw' => false,
         ],
 
+        'spaces' => [
+            'driver' => 's3',
+            'key' => env('DIGITALOCEAN_SPACES_KEY'),
+            'secret' => env('DIGITALOCEAN_SPACES_SECRET'),
+            'endpoint' => env('DIGITALOCEAN_SPACES_ENDPOINT'),
+            'region' => env('DIGITALOCEAN_SPACES_REGION'),
+            'bucket' => env('DIGITALOCEAN_SPACES_BUCKET'),
+        ],
+
     ],
 
     /*
